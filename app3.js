692812147a6e30b8f340604ec4e3b4c43d9e1138
dingxiaolin <dxlani001@163.com>
2018-12-25 15:13:33 +0800

    分命名空间

--- app3.js
+++ app3.js
@@ -1,14 +1,8 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const server=require('http').Server(app);
+const app = require('express')();
+const server=require('http').createServer(app);
 const io=require('socket.io')(server);
-app.use(bodyParser.json());
-app.use(bodyParser.text());
-const _ = require('lodash');
 const Influx = require('influxdb-nodejs');
-// const cors=require ('./cors')
-// cors();
+var async = require("async")
 // const client = new Influx('http://root:root@192.168.1.150:8086,192.168.1.151:8086,192.168.1.152:8086/test');
   const client = new Influx('http://dxl:123456@149.129.97.226:8086/telegraf');
 client.epoch = 'ms';
@@ -16,52 +10,47 @@ client.timeout = 5000;
 client.format = 'json';
 console.info(client.epoch, client.format, client.timeout);
 const table = 'cpu';
-
+var start=""
 client.startHealthCheck();
 
-// app.all('*', function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-//     res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
-//     res.header("Content-Type", "application/json;charset=utf-8");
-//     next();
-// })
-// app.get('/', function (req, res) {
-//     var reader = client.query(table);
-//     reader.start = '-5m';
-//     reader.addCalculate('MEAN', 'usage_iowait'); /* 值的平均值 */
-//     reader.addGroup('time(10s)');
-//     reader.fill = '0';
-//     reader.then(data => {
-//         console.log("send success");
-//         res.send(data);
-//       }).catch(err => {
-//         console.error(err);
-//       });
-//  })
-
- io.on('connection', (socket)=>{
+ io.of('/cpu').on('connection', (socket)=>{
+    socket.on('cpu start', (msg)=>{
+      console.log("msgTime",msg);
+      start=msg;
+      });
    /* 首次连接发送数据 */
    setInterval(()=>{
-      var reader = client.query(table);
-      reader.start = '-30m';
-      reader.addCalculate('MEAN', 'usage_iowait'); /* 值的平均值 */
-      reader.addCalculate('MEAN', 'usage_user'); /* 值的平均值 */
-      reader.addGroup('time(10s)');
-      reader.fill = '0';
-      reader.then(data => {
-          console.log("send success");
-          io.emit('cpu message', data);
-        }).catch(err => {
-          console.error(err);
-        });
-   },5000)
-  //   /* 监听客户端事件 */
-  // socket.on('cpu message', function(msg){
-    
-  // });
+    var reader = client.query(table);
+    reader.start = "-30m";
+    reader.addCalculate('MEAN', 'usage_iowait'); 
+    reader.addCalculate('MEAN', 'usage_user');
+    reader.addGroup('time(10s)');
+    reader.fill = '0';
+    reader.then(data => {
+        console.log("send message success");
+        socket.emit('cpu message', data);
+      }).catch(err => {
+        console.error(err);
+      });
+  },5000)
+   
 });
  
+
+  function getCpuInfo(){
+    var reader = client.query(table);
+    reader.start = start;
+    reader.addCalculate('MEAN', 'usage_iowait'); 
+    reader.addCalculate('MEAN', 'usage_user');
+    reader.addGroup('time(10s)');
+    reader.fill = '0';
+    reader.then(data => {
+        console.log("send message success");
+        socket.emit('cpu message', data);
+      }).catch(err => {
+        console.error(err);
+      });
+  }
 //  var server = app.listen(2019,()=>{
 //     var host = server.address().address;
 //      var port = server.address().port;
